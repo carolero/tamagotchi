cc98324ae8029e4bc309766fc0b6a4e38649077c
Andress <andressaoliveiracintrad@gmail.com>
2019-08-05 16:17:01 -0300

    Serve para criar login, buscar o login

--- src/main/java/com/br/tamagotchi/controllers/LoginController.java
+++ src/main/java/com/br/tamagotchi/controllers/LoginController.java
@@ -1,20 +1,12 @@
 package com.br.tamagotchi.controllers;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import javax.validation.Valid;
-
+import javax.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.validation.BindingResult;
-import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
-
 import com.br.tamagotchi.models.Login;
-import com.br.tamagotchi.models.Usuario;
 import com.br.tamagotchi.services.LoginService;
 
 @Controller
@@ -28,11 +20,26 @@ public class LoginController {
 		ModelAndView modelAndView = new ModelAndView("login.html");
 		return modelAndView;
 	}
-	
+
 	@PostMapping("/login")
-	public String logar(Login login) {
-		ModelAndView modelAndView = new ModelAndView();
+	public String logar(Login login, HttpSession session) {
+		ModelAndView modelAndView = null;
+		if(session.getAttribute("login") != null) {
+			modelAndView = new ModelAndView("redirect" + session.getAttribute("login"));
+		}else {
+			modelAndView = new ModelAndView("login.html");
+		}
+
 		Login objetoLogin = loginService.buscarLogin(login);
+		if ( objetoLogin != null) {
+			session.setAttribute("usuario", loginService.buscarLogin(login));
+			String mensagemDeSaudacao = "Olá, seja bem vindo " + objetoLogin.getApelido(); 
+			modelAndView.addObject("mensagem", mensagemDeSaudacao);
+		}else {
+			String naoEncontradoLogin =  " Não foi encontrado o seu login. Tente novamente ";
+			modelAndView.addObject("mensagem", naoEncontradoLogin);
+		}
+
 		return "redirect:/home";
 	}
 }
