d98d7ddaf492d8425399f76fffd11f184283c23f
Carol <carolina.oliveira@zup.com.br>
2019-08-06 10:28:20 -0300

    Enviar teste de session

--- src/main/java/com/br/tamagotchi/controllers/PetController.java
+++ src/main/java/com/br/tamagotchi/controllers/PetController.java
@@ -1,16 +1,35 @@
 package com.br.tamagotchi.controllers;
 
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.br.tamagotchi.models.Login;
+import com.br.tamagotchi.models.Usuario;
+import com.br.tamagotchi.services.PetService;
+
 @Controller
 public class PetController {
 	
+	@Autowired
+	private PetService petService;
+	
 	@GetMapping("/pet")
-	public ModelAndView exibirFormulario() {
-		ModelAndView mv = new ModelAndView("pagina-pet");
-		return mv;
+	public ModelAndView exibirFormulario(HttpSession session) {
+		if(session.getAttribute("usuario") != null) {
+			ModelAndView modelAndView = new ModelAndView("pagina-pet.html");
+			Login login = (Login) session.getAttribute("login");
+			String mensagem = "Olá " + login.getUsuario().getNome();
+			modelAndView.addObject("mensagem", mensagem);
+			return modelAndView;
+		} else {
+			ModelAndView modelAndView = new ModelAndView("redirect:/login");
+			session.setAttribute("url", "/pet");
+			return modelAndView;
+		}
 	}
 
 }
