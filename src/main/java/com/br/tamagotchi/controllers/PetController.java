ac9ddaa7e4f6747c61da8d62b02e7a63ba5b382a
Brenda <brenda.lima@zup.com.br>
2019-08-09 16:14:29 -0300

    A criação da alimentação do pet

--- src/main/java/com/br/tamagotchi/controllers/PetController.java
+++ src/main/java/com/br/tamagotchi/controllers/PetController.java
@@ -4,16 +4,21 @@ import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.br.tamagotchi.Service.AlimentarPetService;
+import com.br.tamagotchi.models.Pet;
 import com.br.tamagotchi.models.Usuario;
 import com.br.tamagotchi.services.PerguntaService;
 import com.br.tamagotchi.services.UsuarioService;
 
 @Controller
 public class PetController {
-	
+	@Autowired
+	private AlimentarPetService AlimentarPetService;
 	@Autowired
 	private PerguntaService perguntaService;
 	@Autowired
@@ -35,6 +40,15 @@ public class PetController {
 			session.setAttribute("url", "/");
 			return modelAndView;
 		}
+	}
+	
+	@GetMapping("/alimentar")
+	public ModelAndView alimentarPet(HttpSession session) {
+		System.out.println("OLAAAAAA");
+		ModelAndView modelAndView = new ModelAndView("redirect:/");
+		Usuario usuario = (Usuario) session.getAttribute("usuario");
+		AlimentarPetService.alimentarPet(usuario);
+		return modelAndView;
 	}
 
 }
