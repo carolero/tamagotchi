59a6823f92232c92dc83c2d0ee9045f357f5a65e
Natalli <natalli.souza@zup.com.br>
2019-08-12 12:45:58 -0300

    testes

--- src/main/java/com/br/tamagotchi/controllers/LoginController.java
+++ src/main/java/com/br/tamagotchi/controllers/LoginController.java
@@ -1,20 +1,12 @@
 package com.br.tamagotchi.controllers;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import javax.validation.Valid;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.validation.BindingResult;
-import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.br.tamagotchi.models.Login;
-import com.br.tamagotchi.models.Usuario;
 import com.br.tamagotchi.services.LoginService;
 
 @Controller
