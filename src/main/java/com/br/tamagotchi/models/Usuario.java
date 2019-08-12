72b5d5e9506cb3c95164f3b09a139ef6473e8df9
Carol <carolina.oliveira@zup.com.br>
2019-08-12 11:50:15 -0300

    Arrumar aumento de XP do pet ao responder perguntas

--- src/main/java/com/br/tamagotchi/models/Usuario.java
+++ src/main/java/com/br/tamagotchi/models/Usuario.java
@@ -27,7 +27,6 @@ public class Usuario implements Serializable {
 	@Size(min = 4, message = "Nome precisa ter no mínimo 4 letras")
 	private String nome;
 
-	private Integer comida = 0;
 	private Integer level = 0;
 	private Integer pontos = 0;
 	
@@ -61,15 +60,7 @@ public class Usuario implements Serializable {
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
-
-	public Integer getComida() {
-		return comida;
-	}
-
-	public void setComida(Integer comida) {
-		this.comida = comida;
-	}
-
+	
 	public Integer getLevel() {
 		return level;
 	}
